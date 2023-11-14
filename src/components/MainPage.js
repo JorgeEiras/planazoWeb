38a41f26c32e586a9ff84c7b479f886a3f45bb79
Jorge Eiras <j.eiras77@gmail.com>
2023-11-14 05:50:52 -0300

    'Se agrega la rama DEVELOP. Se modifica la grid'

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,32 +6,36 @@ import Content4 from './Content4'
 import Content5 from './Content5'
 import Content6 from './Content6'
 import Content7 from './Content7'
+import Content8 from './Content8'
 
 const MainPage = () => {
   return (
     <React.Fragment>
         <section>
             <div className='layout'>
-                <div className='content1'>
+                <div className='content1 centered'>
                     <Content1/>
                 </div>
-                <div className='content2'>
+                <div className='content2 centered'>
                     <Content2/>
                 </div>
-                <div className='content3'>
+                <div className='content3 centered'>
                     <Content3/>
                 </div>
-                <div className='content4'>
+                <div className='content4 centered text-white'>
                     <Content4/>
                 </div>
-                <div className='content5'>
+                <div className='content5 centered'>
                     <Content5/>
                 </div>
-                <div className='content6'>
+                <div className='content6 centered'>
                     <Content6/>
                 </div>
-                <div className='content7'>
+                <div className='content7 centered'>
                     <Content7/>
+                </div>
+                <div className='content8 centered'>
+                    <Content8/>
                 </div>
             </div>
         </section>
